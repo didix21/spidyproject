d1fe8d7b6073d22ffa3830e170f82e355bd7bfcf
XMabbX <marc.a.c@hotmail.com>
2016-09-26 00:26:19 +0200

    Added more functions, again, and there is a lot of more...

--- Algorithm/CB project/Algorithm/var.h
+++ Algorithm/CB project/Algorithm/var.h
@@ -28,6 +28,8 @@ class gene
 
 class neuron
 {
+    public:
+    neuron();
     std::vector<gene> IncomingVec;
     float value;
 };
@@ -71,6 +73,7 @@ class Pool
 };
 
 Pool::Pool(){
+SpeciesVec.clear();
 generation=0;
 innovation=Outputs;
 currentFrame=0;
@@ -80,6 +83,7 @@ maxFitness=0;
 }
 
 species::species(){
+GenomesVec.clear();
 topFitness=0;
 staleness=0;
 averageFitness=0;
@@ -107,11 +111,42 @@ enabled=true;
 innovation=0;
 }
 
-genome copyGenome(genome);//Funcio que copia dos genomes;
+neuron::neuron(){
+value=0.0;
+}
+
+
+genome copyGenome(genome genomes);//Funcio que copia dos genomes;
 genome basicGenome();//Creates a new genome;
 
-gene copyGene(gene);
+gene copyGene(gene genes);
 
-std::vector<neuron> generateNetwork(genome);
+std::vector<neuron> generateNetwork(genome genomes); //Send a genome to genrate the NN
+
+void evaluateNetwork(std::vector<neuron> network,float * Inputs,float * Outputs); //Calculate the output of a network
+
+genome crossover(genome g1,genome g2);
+
+int randomNeuron(gene genes,bool nonInput);
+
+bool existLink(gene genes,gene link);
+
+void pointMutate(genome genomes);
+
+std::vector<gene> linkMutate(genome genomes, bool forceBias);
+
+std::vector<gene> nodeMutate(genome genomes);
+
+bool enbaleDisableMutate(genome genomes, bool enable);
+
+genome mutate(genome genomes);
+
+float disjoint(gene genes1,gene genes2);
+
+float weights(gene genes1,gene genes2);
+
+bool sameSpecies(genome genome1,genome genome2);
+
+
 
 #endif // VAR_H_INCLUDED
