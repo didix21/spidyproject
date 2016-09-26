9c2ed4f824f177ff4011f8325659ddc755286720
XMabbX <marc.a.c@hotmail.com>
2016-09-27 00:14:02 +0200

    Added things

--- Algorithm/CB project/Algorithm/main.cpp
+++ Algorithm/CB project/Algorithm/main.cpp
@@ -8,17 +8,33 @@ using namespace std;
 string InputsNames[] = {"Servi1","Servi2","Servi3","Servi4"};
 string OutputsNames[] = {"Servo1","Servo2","Servo3","Servo4"};
 
+void Pool::addToSpecies(genome child){
+
+}
+
 void Pool::initializePool()
 {
-    for(int i;Population;i++)
+    for(int i=0;i<Population;++i)
     {
-
+        genome basic = basicGenome();
+        Pool::addToSpecies(basic);
+        //cout << i;
     }
+}
+
+genome basicGenome()
+{
+    genome genome;
+    //innovation = 1;
+    genome.maxneuron = Inputs;
+    return genome;
 }
 
 int main()
 {
     Pool pool;
+
+    pool.initializePool();
 
     //cout << "Hello world!" << endl;
     //cout << pool.currentFrame;
