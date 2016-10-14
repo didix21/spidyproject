f59d2a85159622da8dd7016172e1ce54826e05de
didix21 <didaccoll_93@hotmail.com>
2016-10-14 12:56:05 +0200

    Menor changes in spidyfirmware

--- Arduino_Programs/SpidyFirmware/configuration.h
+++ Arduino_Programs/SpidyFirmware/configuration.h
@@ -3,7 +3,9 @@
  */
 
 #ifndef _CONFIGURATION_H_
+
   #define _CONFIGURATION_H_
+  
  /**
   * L = Leg, Nº is the leg number.
   * S = Servo, Nº is the servo number: 1 is the servo near the paw and 2 is the base servo
@@ -23,8 +25,6 @@
    #define L5S2_PIN 12  // Leg 5 Servo 2
    #define L6S1_PIN 13  // Leg 6 Servo 1
    #define L6S2_PIN 14  // Leg 6 Servo 2
-   
-   //#define LNSN (L1S1, L1S2, L2S1, L2S2, L3S1, L3S2, L4S1, L4S2, L5S1, L5S2, L6S1, L6S2)
   
   #endif // LNSN
   // ***********************************************************************************
@@ -79,9 +79,6 @@
   #define L4S2_REST (L4S2_MAX - L4S2_MIN)/2
   #define L5S2_REST (L5S2_MAX - L5S2_MIN)/2
   #define L6S2_REST (L1S2_MAX - L6S2_MIN)/2
-
-  //#define LNSN_REST (L1S1_MIN, L1S2_REST, L2S1_MIN, L2S2_REST, L3S1_MIN, L3S2_REST \
-    //                 L4S1_MIN, L4S2_REST, L5S1_MIN, L5S2_REST, L6S1_MIN, L6S2_REST)
                      
   #define RANGE_OF_EACH_SERVO
 
