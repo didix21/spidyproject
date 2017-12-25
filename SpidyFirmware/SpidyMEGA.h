40374924c5e9a7a58012eb9a625116afc9e17395
Dídac Coll Pujals <didaccoll_93@hotmail.com>
2017-12-25 22:02:04 +0100

    modify spidymega

--- SpidyFirmware/SpidyMEGA.h
+++ SpidyFirmware/SpidyMEGA.h
@@ -17,7 +17,7 @@
      * Public Methods
      */
     public:
-      Spidy();
+      SpidyMEGA();
       void attachServos();
       void setSpidyRest();
       void setSpidyUp();
@@ -33,14 +33,6 @@
       Servo l1s1, l1s2, l2s1, l2s2, l3s1, l3s2, \
             l4s1, l4s2, l5s1, l5s2, l6s1, l6s2;
 
-    /**
-     * Private Variables
-     */
-     private:
-       uint8_t minPos[12]={L1S1_MIN,L1S2_MIN,L2S1_MIN,L2S2_MIN,L3S1_MIN,L3S2_MIN, \
-                            L4S1_MIN,L4S2_MIN,L5S1_MIN,L5S2_MIN,L6S1_MIN,L6S2_MIN};
-       uint8_t maxPos[12]={L1S1_MAX,L1S2_MAX,L2S1_MAX,L2S2_MAX,L3S1_MAX,L3S2_MAX, \
-                            L4S1_MAX,L4S2_MAX,L5S1_MAX,L5S2_MAX,L6S1_MAX,L6S2_MAX};
 
   };
 
