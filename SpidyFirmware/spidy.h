ae2aa82262cfbc133fec1e0bfcae5a6fcec545b9
Dídac Coll Pujals <didaccoll_93@hotmail.com>
2017-12-25 18:34:08 +0100

    modified spidy.h

--- SpidyFirmware/spidy.h
+++ SpidyFirmware/spidy.h
@@ -8,16 +8,16 @@
   #include "Arduino.h"
   #include <Servo.h>
   #include "configuration.h"
-  
-  
+
+
   class Spidy {
 
-      
+
     /**
      * Public Methods
      */
     public:
-      Spidy(); 
+      Spidy();
       void attachServos();
       void setSpidyRest();
       void setSpidyUp();
@@ -27,14 +27,14 @@
       void readLegsPosition(uint8_t *legsAngle);
       bool *checkPosition(uint8_t *legsAngle, uint8_t *legsPosition);
 
-      
+
     /**
      * Public variables
      */
     public:
       Servo l1s1, l1s2, l2s1, l2s2, l3s1, l3s2, \
-            l4s1, l4s2, l5s1, l5s2, l6s1, l6s2;  
-            
+            l4s1, l4s2, l5s1, l5s2, l6s1, l6s2;
+
     /**
      * Private Variables
      */
@@ -43,7 +43,7 @@
                             L4S1_MIN,L4S2_MIN,L5S1_MIN,L5S2_MIN,L6S1_MIN,L6S2_MIN};
        uint8_t maxPos[12]={L1S1_MAX,L1S2_MAX,L2S1_MAX,L2S2_MAX,L3S1_MAX,L3S2_MAX, \
                             L4S1_MAX,L4S2_MAX,L5S1_MAX,L5S2_MAX,L6S1_MAX,L6S2_MAX};
-   
+
   };
 
-#endif _SPIDY_H_
+#endif //_SPIDY_H_
