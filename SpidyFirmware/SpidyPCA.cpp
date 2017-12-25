b9ce7163de79f7dfaff3dae113f879bc04d7d45f
Dídac Coll Pujals <didaccoll_93@hotmail.com>
2017-12-25 18:40:34 +0100

    comment spidypca

--- SpidyFirmware/SpidyPCA.cpp
+++ SpidyFirmware/SpidyPCA.cpp
@@ -1,20 +1,19 @@
-
-#include "SpidyPCA.cpp"
-
-SpidyPCA::SpidyPCA() {
-
-  
-}
-
-void SpidyPCA::wireConfiguration()
-{
-  Wire.begin();
-  Wire.setClock(WIRE_BAUDRATE); 
-}
-
-void SpidyPCA::pcaConfig()
-{
-  pwmController.resetDevices();
-  pwmController.init(I2C_ADDRESS);
-}
-
+// 
+// #include "SpidyPCA.cpp"
+//
+// SpidyPCA::SpidyPCA() {
+//
+//
+// }
+//
+// void SpidyPCA::wireConfiguration()
+// {
+//   Wire.begin();
+//   Wire.setClock(WIRE_BAUDRATE);
+// }
+//
+// void SpidyPCA::pcaConfig()
+// {
+//   pwmController.resetDevices();
+//   pwmController.init(I2C_ADDRESS);
+// }
