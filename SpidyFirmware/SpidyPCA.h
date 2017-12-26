a38f722b085cd238f6bb73d2416f3092e7cec361
Dídac Coll Pujals <didaccoll_93@hotmail.com>
2017-12-26 03:25:24 +0100

    Modifications in PCA

--- SpidyFirmware/SpidyPCA.h
+++ SpidyFirmware/SpidyPCA.h
@@ -1,26 +1,31 @@
-// 
-//
-//
-// #ifndef _SPIDYPCA_H_
-//   #define _SPIDY_PCA_H_
-//
-//
-//   #include <Wire.h>
-//   #include "PCA9685.h"
-//
-//   class SpidyPCA {
-//
-//     public:
-//       void  wireConfiguration(); // Configures the Wire.
-//       void  pcaConfig();         // Configurates the PCA.
-//
-//     public:
-//       PCA9685 pwmController;
-//       PCA9685_ServoEvaluator servos;
-//
-//
-//
-//   };
-//
-//
-// #endif // _SPIDYPCA_H_
+
+#ifdef PCA9685_BOARD
+
+#ifndef _SPIDYPCA_H_
+  #define _SPIDY_PCA_H_
+
+
+  #include <Wire.h>
+  #include "PCA9685.h"
+
+  class SpidyPCA {
+
+    public:
+      SpidyPCA();
+      void wireConfiguration(); // Configures the Wire.
+      void pcaConfig();         // Configurates the PCA.
+      void setSpidyRest();
+      void setSpidyUp();
+      void setSpidyDown();
+      void refreshLegs(uint8_t *legsAngle);
+
+
+    public:
+      PCA9685 pwmController;
+      PCA9685_ServoEvaluator servos;
+  };
+
+
+#endif // _SPIDYPCA_H_
+
+#endif // PCA9685_BOARD
